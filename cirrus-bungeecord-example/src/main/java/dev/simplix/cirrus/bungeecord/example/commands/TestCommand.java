0ebdb4738b3694849381ab8d4d52dc213fec4ac2
Nico Britze <n.britze@exceptionflug.de>
2020-09-06 17:50:51 +0200

    test

--- cirrus-bungeecord-example/src/main/java/dev/simplix/cirrus/bungeecord/example/commands/TestCommand.java
+++ cirrus-bungeecord-example/src/main/java/dev/simplix/cirrus/bungeecord/example/commands/TestCommand.java
@@ -36,6 +36,7 @@ public class TestCommand extends Command {
               "plugins/Cirrus/example.json",
               MultiPageMenuConfiguration.class)).open(menuBuilder);
     }
+    https://github.com/Exceptionflug/protocolize/releases/download/v1.6.3/protocolize-plugin.jar
   }
 
 }
