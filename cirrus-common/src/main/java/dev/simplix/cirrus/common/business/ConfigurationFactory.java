e5ea7c2c13571a8488b5307413c3a834da6540a4
Leonhard <javafactorydev@gmail.com>
2021-12-26 22:35:51 +0100

    Add convenience methods

--- cirrus-common/src/main/java/dev/simplix/cirrus/common/business/ConfigurationFactory.java
+++ cirrus-common/src/main/java/dev/simplix/cirrus/common/business/ConfigurationFactory.java
@@ -1,7 +1,9 @@
 package dev.simplix.cirrus.common.business;
 
 import dev.simplix.cirrus.common.configuration.MenuConfiguration;
+import dev.simplix.cirrus.common.configuration.MultiPageMenuConfiguration;
 import dev.simplix.cirrus.common.configuration.impl.SimpleMenuConfiguration;
+import dev.simplix.cirrus.common.configuration.impl.SimpleMultiPageMenuConfiguration;
 import lombok.NonNull;
 
 public interface ConfigurationFactory {
@@ -12,6 +14,16 @@ public interface ConfigurationFactory {
 
     default MenuConfiguration loadResource(@NonNull String resourcePath, @NonNull Class<?> caller) {
         return loadResource(resourcePath, caller, SimpleMenuConfiguration.class);
+    }
+
+    default MultiPageMenuConfiguration loadFileForMultiPageMenu(@NonNull String file) {
+        return loadFile(file, SimpleMultiPageMenuConfiguration.class);
+    }
+
+    default MultiPageMenuConfiguration loadResourcesForMultiPageMenu(
+            @NonNull String resourcePath,
+            @NonNull Class<?> caller) {
+        return loadResource(resourcePath, caller, SimpleMultiPageMenuConfiguration.class);
     }
 
     <T extends MenuConfiguration> T loadFile(@NonNull String file, @NonNull Class<T> type);
