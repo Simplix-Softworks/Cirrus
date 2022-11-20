14e8db000ad23070fa6aec6be3fb29a66ea82f9d
Leonhard S <49833472+KotlinFactory@users.noreply.github.com>
2022-11-20 16:18:15 +0100

    Remove unused method

--- cirrus-api/src/main/java/dev/simplix/cirrus/Utils.java
+++ cirrus-api/src/main/java/dev/simplix/cirrus/Utils.java
@@ -1,5 +1,7 @@
 package dev.simplix.cirrus;
 
+import dev.simplix.protocolize.api.Protocolize;
+import dev.simplix.protocolize.data.ItemType;
 import dev.simplix.protocolize.data.inventory.InventoryType;
 import java.util.*;
 import lombok.NonNull;
@@ -10,12 +12,6 @@ import net.querz.nbt.tag.*;
 public class Utils {
 
   private static final String STEVE_TEXTURE = "ewogICJ0aW1lc3RhbXAiIDogMTU5MTU3NDcyMzc4MywKICAicHJvZmlsZUlkIiA6ICI4NjY3YmE3MWI4NWE0MDA0YWY1NDQ1N2E5NzM0ZWVkNyIsCiAgInByb2ZpbGVOYW1lIiA6ICJTdGV2ZSIsCiAgInNpZ25hdHVyZVJlcXVpcmVkIiA6IHRydWUsCiAgInRleHR1cmVzIiA6IHsKICAgICJTS0lOIiA6IHsKICAgICAgInVybCIgOiAiaHR0cDovL3RleHR1cmVzLm1pbmVjcmFmdC5uZXQvdGV4dHVyZS82ZDNiMDZjMzg1MDRmZmMwMjI5Yjk0OTIxNDdjNjlmY2Y1OWZkMmVkNzg4NWY3ODUwMjE1MmY3N2I0ZDUwZGUxIgogICAgfSwKICAgICJDQVBFIiA6IHsKICAgICAgInVybCIgOiAiaHR0cDovL3RleHR1cmVzLm1pbmVjcmFmdC5uZXQvdGV4dHVyZS85NTNjYWM4Yjc3OWZlNDEzODNlNjc1ZWUyYjg2MDcxYTcxNjU4ZjIxODBmNTZmYmNlOGFhMzE1ZWE3MGUyZWQ2IgogICAgfQogIH0KfQ==";
-
-//  public void removeItalic(BaseComponent[] components) {
-//    for (BaseComponent component : components) {
-//      component.setItalic(false);
-//    }
-//  }
 
   public Optional<UUID> fromString(final String uuidString) {
     if (uuidString == null || uuidString.isEmpty()) {
