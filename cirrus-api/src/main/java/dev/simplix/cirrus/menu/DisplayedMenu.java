ccdc44a491f0c36fc9c667b714334750878836a4
Leonhard S <49833472+KotlinFactory@users.noreply.github.com>
2022-12-12 12:19:03 +0100

    Add documentation for DisplayMenu

--- cirrus-api/src/main/java/dev/simplix/cirrus/menu/DisplayedMenu.java
+++ cirrus-api/src/main/java/dev/simplix/cirrus/menu/DisplayedMenu.java
@@ -5,6 +5,9 @@ import dev.simplix.cirrus.service.MenuBuildService;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+/**
+ * This class represents a displayed menu.
+ */
 public record DisplayedMenu(
     Menu value,
     Object nativeMenu,
@@ -15,6 +18,15 @@ public record DisplayedMenu(
 
 ) {
 
+  /**
+   * Creates a new instance of {@code DisplayedMenu}.
+   *
+   * @param value the menu itself
+   * @param nativeMenu the native menu object
+   * @param player the player who sees the menu
+   * @param menuBuildService the menu build service
+   * @param id the id of the menu
+   */
   public DisplayedMenu(
       Menu value,
       Object nativeMenu,
@@ -25,19 +37,38 @@ public record DisplayedMenu(
     Menus.register(player.uuid(), this);
   }
 
+  /**
+   * Updates the menu.
+   */
   public void update() {
     this.menuBuildService.updateMenu(this);
   }
 
+
+  /**
+   * Closes the menu.
+   */
   public void close() {
     this.menuBuildService.closeMenu(this);
   }
 
+
+  /**
+   * Returns the hash code for this menu.
+   *
+   * @return the hash code for this menu
+   */
   @Override
   public int hashCode() {
     return Objects.hash(value, menuBuildService, id);
   }
 
+  /**
+   * Indicates whether some other object is "equal to" this menu.
+   *
+   * @param other the reference object with which to compare
+   * @return {@code true} if this menu is the same as the {@code other} argument; {@code false} otherwise
+   */
   @Override
   public boolean equals(Object other) {
     if (other == null) {
